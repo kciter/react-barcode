402a22b3221d769ae08868897cb5bfc68744fd34
Cole Panike <cole.panike@walmart.com>
2019-11-15 14:19:52 -0600

    Fix #38
    New react version causes trigger of wrong version detection due to lack of parentheticals

--- src/react-barcode.js
+++ src/react-barcode.js
@@ -8,7 +8,7 @@ var getDOMNode;
 // Super naive semver detection but it's good enough. We support 0.12, 0.13
 // which both have getDOMNode on the ref. 0.14 and 15 make the DOM node the ref.
 var version = React.version.split(/[.-]/);
-if (version[0] === '0' && version[1] === '13' || version[1] === '12') {
+if (version[0] === '0' && (version[1] === '13' || version[1] === '12')) {
   getDOMNode = (ref) => ref.getDOMNode();
 } else {
   getDOMNode = (ref) => ref;
@@ -23,15 +23,15 @@ class Barcode extends Component {
     shouldComponentUpdate(nextProps) {
       return Object.keys(Barcode.propTypes).some((k) => this.props[k] !== nextProps[k]);
     };
-  
+
     componentDidMount() {
       this.update();
-    }; 
-  
+    };
+
     componentDidUpdate() {
       this.update();
     };
-  
+
     update() {
       var renderElement = getDOMNode(this.refs.renderElement);
       try {
