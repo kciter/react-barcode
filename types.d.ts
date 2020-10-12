ea883858e0c9c0d4804920811abbd9bf2861ab8f
Anılcan Metinyurt <igoodie@programmer.net>
2020-10-12 12:31:02 +0300

    feat(types): add Typescript types

---
+++ types.d.ts
@@ -0,0 +1,32 @@
+declare module "react-barcode" {
+  declare type Options = {
+    width?: number;
+    height?: number;
+    format?:
+      | "CODE39"
+      | "CODE128"
+      | "EAN13"
+      | "ITF14"
+      | "MSI"
+      | "pharmacode"
+      | "codabar";
+    displayValue?: boolean;
+    fontOptions?: string;
+    font?: string;
+    textAlign?: string;
+    textPosition?: string;
+    textMargin?: number;
+    fontSize?: number;
+    background?: string;
+    lineColor?: string;
+    margin?: number;
+    marginTop?: number;
+    marginBottom?: number;
+    marginLeft?: number;
+    marginRight?: number;
+  };
+
+  declare const ReactBarcode: React.FunctionComponent<Options>;
+
+  export = ReactBarcode;
+}
