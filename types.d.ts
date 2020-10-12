e1106561aac73fd55314ada9f48fa4055415d38c
Anılcan Metinyurt <igoodie@programmer.net>
2020-10-12 13:38:43 +0300

    fix(types): add missing "value" prop

--- types.d.ts
+++ types.d.ts
@@ -1,32 +1,38 @@
 declare module "react-barcode" {
-  declare type Options = {
-    width?: number;
-    height?: number;
-    format?:
-      | "CODE39"
-      | "CODE128"
-      | "EAN13"
-      | "ITF14"
-      | "MSI"
-      | "pharmacode"
-      | "codabar";
-    displayValue?: boolean;
-    fontOptions?: string;
-    font?: string;
-    textAlign?: string;
-    textPosition?: string;
-    textMargin?: number;
-    fontSize?: number;
-    background?: string;
-    lineColor?: string;
-    margin?: number;
-    marginTop?: number;
-    marginBottom?: number;
-    marginLeft?: number;
-    marginRight?: number;
-  };
+  declare namespace ReactBarcode {
+    declare type Options = {
+      width?: number;
+      height?: number;
+      format?:
+        | "CODE39"
+        | "CODE128"
+        | "EAN13"
+        | "ITF14"
+        | "MSI"
+        | "pharmacode"
+        | "codabar";
+      displayValue?: boolean;
+      fontOptions?: string;
+      font?: string;
+      textAlign?: string;
+      textPosition?: string;
+      textMargin?: number;
+      fontSize?: number;
+      background?: string;
+      lineColor?: string;
+      margin?: number;
+      marginTop?: number;
+      marginBottom?: number;
+      marginLeft?: number;
+      marginRight?: number;
+    };
 
-  declare const ReactBarcode: React.FunctionComponent<Options>;
+    declare type Props = {
+      value: string;
+    } & ReactBarcode.Options;
+  }
+
+  declare const ReactBarcode: React.FunctionComponent<Props>;
 
   export = ReactBarcode;
 }
