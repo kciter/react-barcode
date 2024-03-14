d9b1fde29bea627b1d491a7c422dc3880e1da309
MinatoWada <min0303szsz@gmail.com>
2024-03-15 08:08:14 +0900

    Supported all formats available in JsBarcode

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,13 +7,25 @@ export interface Options {
   format?:
     | "CODE39"
     | "CODE128"
+    | "CODE128A"
+    | "CODE128B"
+    | "CODE128C"
     | "EAN13"
+    | "EAN8"
+    | "EAN5"
+    | "EAN2"
+    | "UPC"
+    | "UPCE"
     | "ITF14"
     | "ITF"
     | "MSI"
+    | "MSI10"
+    | "MSI11"
+    | "MSI1010"
+    | "MSI1110"
     | "pharmacode"
     | "codabar"
-    | "upc";
+    | "GenericBarcode";
   displayValue?: boolean;
   fontOptions?: string;
   font?: string;
